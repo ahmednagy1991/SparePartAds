5f36e86ecad81adf7a980eef16b71a5f26731b16
ahmed nagy <ahmed_nagy1991@hotmail.com>
2019-05-03 15:38:17 +0200

    cors

--- middleware/anonyms.js
+++ middleware/anonyms.js
@@ -1,8 +1,12 @@
 
 module.exports = function (req, res, next) {
     console.log("enabling cors");
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Headers', 'x-user-auth');
+    res.setHeader('Cache-Control', 'no-cache');
     
     next();
    
