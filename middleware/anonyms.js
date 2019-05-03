a81785d95c913c935dabc54c927cfd4225e45983
ahmed nagy <ahmed_nagy1991@hotmail.com>
2019-05-03 22:14:04 +0200

    fix

--- middleware/anonyms.js
+++ middleware/anonyms.js
@@ -1,9 +1,8 @@
 
 module.exports = function (req, res, next) {
+    
     console.log("enabling cors");
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Origin', '*');  
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Headers', 'x-user-auth');
     res.setHeader('Cache-Control', 'no-cache');
--- routes/category.js
+++ routes/category.js
@@ -12,6 +12,7 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', anony,(req, res) => {
+    console.log("calling post category");
     const error = category_schema.validate(req.body);
     if (error.error) return res.status(400).send(error.error.details[0].message);
     category.create(req.body).then((result) => {
