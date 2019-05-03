f071d9a460b5408151d62e78d423845d9fd7d678
ahmed nagy <ahmed_nagy1991@hotmail.com>
2019-05-03 23:02:49 +0200

    fix

--- routes/category.js
+++ routes/category.js
@@ -11,7 +11,7 @@ router.get('/', async (req, res) => {
     return res.send(cats);   
 });
 
-router.post('/', anony,(req, res) => {
+router.post('/',(req, res) => {
     console.log("calling post category");
     const error = category_schema.validate(req.body);
     if (error.error) return res.status(400).send(error.error.details[0].message);
