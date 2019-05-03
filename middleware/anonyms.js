bba51a39bdedcd5e4410d2e22074a171c96c72ca
ahmed nagy <ahmed_nagy1991@hotmail.com>
2019-05-03 15:07:32 +0200

    fixing cors

--- middleware/anonyms.js
+++ middleware/anonyms.js
@@ -1,6 +1,6 @@
 
 module.exports = function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Headers', 'x-user-auth');
     res.setHeader('Cache-Control', 'no-cache');
